eb365e6a56571ebe5d9e1d06571403e9c98d0a19
pokoivuu <pauli.o.koivuniemi@student.jyu.fi>
2021-02-17 17:38:57 +0200

    muokkauksia

--- src/fxRekisteri/RekisteriMain.java
+++ src/fxRekisteri/RekisteriMain.java
@@ -2,6 +2,7 @@ package fxRekisteri;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.fxml.FXMLLoader;
@@ -18,11 +19,14 @@ public class RekisteriMain extends Application {
         try {
             FXMLLoader ldr = new FXMLLoader(getClass().getResource("RekisteriGUIView.fxml"));
             final Pane root = ldr.load();
+            final RekisteriGUIController kerhoCtrl = (RekisteriGUIController)ldr.getController();
+            
             Scene scene = new Scene(root);
             scene.getStylesheets().add(getClass().getResource("rekisteri.css").toExternalForm());
             primaryStage.setScene(scene);
             primaryStage.setTitle("saarekisteri");
             primaryStage.show();
+            if ( !kerhoCtrl.avaa() ) Platform.exit();
         } catch(Exception e) {
             e.printStackTrace();
         }
