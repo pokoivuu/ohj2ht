deeab1d9beb202f930d1fb68f7eef4a3b96a1fdb
Kupiainen Teemu Ilmari <teemu.i.kupiainen@student.jyu.fi>
2021-02-17 13:32:53 +0200

    vaihe3

--- src/fxRekisteri/RekisteriGUIController.java
+++ src/fxRekisteri/RekisteriGUIController.java
@@ -1,10 +1,54 @@
+/**
+ * Sample Skeleton for 'RekisteriGUIView.fxml' Controller Class
+ */
+
 package fxRekisteri;
+
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import fi.jyu.mit.fxgui.ComboBoxChooser;
+import fi.jyu.mit.fxgui.Dialogs;
+import fi.jyu.mit.fxgui.ModalController;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 
 /**
  * @author Teemu Kupiainen, Pauli Koivuniemi
- * @version 21 Jan 2021
+ * @version 17 Feb 2021
  *
  */
 public class RekisteriGUIController {
-	//
+    @FXML private TextField HakuField;
+    @FXML private ComboBoxChooser<String> cbPaikka;
+    
+    private String rekisterinimi = "2020-2021";
+    
+    /**
+     * @param url ?
+     * @param bundle ?
+     */
+    public void initialize(URL url, ResourceBundle bundle) {
+        //      
+    }
+    
+
+    @FXML private void handleHaku () {
+        String haku = cbPaikka.getSelectedText();
+        
+    }
+    
+    
+    @FXML private void handleMenuTietoja() {        
+        ModalController.showModal(RekisteriGUIController.class.getResource("TietoView.fxml"), "saarekisteri", null, "");
+    }
+
 }
--- src/fxRekisteri/RekisteriMain.java
+++ src/fxRekisteri/RekisteriMain.java
@@ -33,3 +33,4 @@ public class RekisteriMain extends Application {
 		launch(args);
 	}
 }
+
